e39546dd5ab7df7a3f9b6c34fd5e1d615292c0b4
Eshan Mohammad <eshan@Eshans-MacBook-Air.local>
2022-07-06 00:44:40 -0700

    adding calculator app

--- M1-Challenge-Mohammad-Ruksar Jaha/CalculatorObject/src/main/java/com/company/com/company/CalculatorApp.java
+++ M1-Challenge-Mohammad-Ruksar Jaha/CalculatorObject/src/main/java/com/company/com/company/CalculatorApp.java
@@ -1,5 +1,30 @@
 package com.company.com.company;
 
+import com.company.Calculator;
+
 public class CalculatorApp {
 
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        calculator.add( 1, 1);
+        calculator.add( 3.4, 2.3);
+        calculator.multiply( 34, 2);
+        calculator.multiply( 6.7, 4.4);
+        calculator.subtract(23, 52);
+        calculator.subtract( 5.5, 0.5);
+        calculator.division(12, 3);
+        calculator.division( 12, 7);
+        calculator.division( 10.8, 2.2);
+
+        System.out.println(calculator.add(1, 1));
+        System.out.println(calculator.multiply(34, 2));
+        System.out.println(calculator.subtract(23, 52));
+        System.out.println(calculator.division(12, 3));
+        System.out.println(calculator.add(3.4, 2.3));
+        System.out.println(calculator.multiply(6.7, 4.4));
+        System.out.println(calculator.subtract(5.5, 0.5));
+        System.out.println(calculator.division(12, 7));
+        System.out.println(calculator.division(10.8, 2.2));
+    }
 }
--- M1-Challenge-Mohammad-Ruksar Jaha/IceCreamClassDesign/src/main/java/com/company/factory/IceCream.java
+++ M1-Challenge-Mohammad-Ruksar Jaha/IceCreamClassDesign/src/main/java/com/company/factory/IceCream.java
@@ -43,6 +43,6 @@ public class IceCream {
                 '}';
     }
 
-    
+
 
 }
